93e69bf95f856c91c88d5163e40c4e150381d212
blenderistDev <demonx.blenderist@gmail.com>
2021-08-28 23:32:17 +0300

    Перевел форму логина на pug

--- frontend/src/components/Login.vue
+++ frontend/src/components/Login.vue
@@ -1,14 +1,13 @@
-<template>
-  <q-form>
-    <q-input
-      v-model="phone"
-      type="tel"
-    >
-    </q-input>
-  </q-form>
+<template lang="pug">
+QForm
+  QInput(
+    v-model="phone"
+    type="tel"
+  )
 </template>
 
 <script lang="ts">
+import { QInput, QForm } from 'quasar';
 import {
   defineComponent,
   ref,
@@ -16,6 +15,10 @@ import {
 
 export default defineComponent({
   name: 'CompositionComponent',
+  components: {
+    QInput,
+    QForm,
+  },
   setup() {
     const phone = ref('');
     return {
