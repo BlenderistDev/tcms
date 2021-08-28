b170b400682240111cc845291a24993656535ba3
blenderistDev <demonx.blenderist@gmail.com>
2021-08-28 23:36:31 +0300

    Удалил лишний код с индексной страницы. Перевел шаблон на pug

--- frontend/src/pages/Index.vue
+++ frontend/src/pages/Index.vue
@@ -1,49 +1,15 @@
-<template>
-  <q-page class="row items-center justify-evenly">
-    <login
-      title="Example component"
-      active
-      :todos="todos"
-      :meta="meta"
-    ></Login>
-  </q-page>
+<template lang="pug">
+QPage.row.items-center.justify-evenly
+  Login
 </template>
 
 <script lang="ts">
-import { Todo, Meta } from 'components/models';
-import { defineComponent, ref } from 'vue';
+import { defineComponent } from 'vue';
+import { QPage } from 'quasar';
 import Login from '../components/Login.vue';
 
 export default defineComponent({
   name: 'PageIndex',
-  components: { Login },
-  setup() {
-    const todos = ref<Todo[]>([
-      {
-        id: 1,
-        content: 'ct1',
-      },
-      {
-        id: 2,
-        content: 'ct2',
-      },
-      {
-        id: 3,
-        content: 'ct3',
-      },
-      {
-        id: 4,
-        content: 'ct4',
-      },
-      {
-        id: 5,
-        content: 'ct5',
-      },
-    ]);
-    const meta = ref<Meta>({
-      totalCount: 1200,
-    });
-    return { todos, meta };
-  },
+  components: { Login, QPage },
 });
 </script>
