81057bd6795154ed4b0ae245f14e9a27905ae52b
blenderistDev <demonx.blenderist@gmail.com>
2021-08-31 22:01:44 +0300

    Вынес вебсервер в отдельный package

--- backend/tcms.go
+++ backend/tcms.go
@@ -4,19 +4,10 @@ import (
 	"fmt"
 	"os"
 	"tcms/m/telegram"
+	"tcms/m/webserver"
 
-	"github.com/gin-contrib/cors"
-	"github.com/gin-gonic/gin"
 	"github.com/shelomentsevd/mtproto"
 )
-
-type loginData struct {
-	Phone string `json:"phone" binding:"required"`
-}
-
-type signData struct {
-	Code string `json:"code" binding:"required"`
-}
 
 func main() {
 	wd, err := os.Getwd()
@@ -38,32 +29,5 @@ func main() {
 		fmt.Println(err)
 	}
 
-	router := gin.Default()
-
-	router.Use(cors.New(cors.Config{
-		AllowOrigins:  []string{"*"},
-		AllowMethods:  []string{"GET", "POST"},
-		AllowHeaders:  []string{"Origin", "Content-Type"},
-		ExposeHeaders: []string{"Content-Length"},
-	}))
-
-	var sentCode *mtproto.TL_auth_sentCode
-	router.POST("/login", func(c *gin.Context) {
-		var loginData loginData
-		c.BindJSON(&loginData)
-		sentCode, err = telegram.Authorization(loginData.Phone)
-		if err != nil {
-			fmt.Println(err)
-		}
-		c.JSON(200, gin.H{"status": "ok"})
-	})
-
-	router.POST("/sign", func(c *gin.Context) {
-		var signData signData
-		c.BindJSON(&signData)
-		telegram.AuthSignIn(signData.Code, sentCode)
-		c.JSON(200, gin.H{"status": "ok"})
-	})
-
-	router.Run(":8080")
+	webserver.StartWebServer(telegram)
 }
---
+++ backend/webserver/webserver.go
@@ -0,0 +1,50 @@
+package webserver
+
+import (
+	"fmt"
+	"tcms/m/telegram"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+	"github.com/shelomentsevd/mtproto"
+)
+
+type loginData struct {
+	Phone string `json:"phone" binding:"required"`
+}
+
+type signData struct {
+	Code string `json:"code" binding:"required"`
+}
+
+func StartWebServer(telegram *telegram.Telegram) {
+	router := gin.Default()
+
+	router.Use(cors.New(cors.Config{
+		AllowOrigins:  []string{"*"},
+		AllowMethods:  []string{"GET", "POST"},
+		AllowHeaders:  []string{"Origin", "Content-Type"},
+		ExposeHeaders: []string{"Content-Length"},
+	}))
+
+	var sentCode *mtproto.TL_auth_sentCode
+	router.POST("/login", func(c *gin.Context) {
+		var loginData loginData
+		c.BindJSON(&loginData)
+		var err error
+		sentCode, err = telegram.Authorization(loginData.Phone)
+		if err != nil {
+			fmt.Println(err)
+		}
+		c.JSON(200, gin.H{"status": "ok"})
+	})
+
+	router.POST("/sign", func(c *gin.Context) {
+		var signData signData
+		c.BindJSON(&signData)
+		telegram.AuthSignIn(signData.Code, sentCode)
+		c.JSON(200, gin.H{"status": "ok"})
+	})
+
+	router.Run(":8080")
+}
