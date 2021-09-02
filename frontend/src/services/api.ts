15b87ee35e70759f40eead0cadce8484b28f08d1
blenderistDev <demonx.blenderist@gmail.com>
2021-09-02 22:27:25 +0300

    Добавил запрос для получения данных о пользователе

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,5 +1,12 @@
 import { AxiosResponse } from 'axios';
 import { api } from 'src/boot/axios';
+import { User } from 'src/store/module-example/state';
 
 export const login = (phone: string):Promise<AxiosResponse> => api.post('/login', { phone });
 export const sign = (code: string):Promise<AxiosResponse> => api.post('/sign', { code });
+
+export interface user {
+    id: number
+}
+
+export const getUser = () => api.get<User>('/me').then((response) => response.data);
