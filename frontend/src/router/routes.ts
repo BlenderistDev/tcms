75d62447765acf8fa70f3691652e0a6fae104213
BlenderistDev <demonx.blenderist@gmail.com>
2021-09-03 12:25:42 +0300

    Добавил названия на поля формы логина

--- frontend/src/router/routes.ts
+++ frontend/src/router/routes.ts
@@ -4,9 +4,17 @@ const routes: RouteRecordRaw[] = [
   {
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
-    children: [{ path: '', component: () => import('pages/Index.vue') }],
+    children: [
+      {
+        path: '',
+        component: () => import('pages/Index.vue'),
+      },
+      {
+        path: 'login',
+        component: () => import('pages/Login.vue'),
+      },
+    ],
   },
-
   // Always leave this as last one,
   // but you can also remove it
   {
