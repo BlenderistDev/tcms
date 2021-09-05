4471cc8bba561a0a16137287203815765e8a1675
blenderistDev <demonx.blenderist@gmail.com>
2021-09-05 12:30:37 +0300

    Вывел таблицу с контактами

--- frontend/src/components/Contacts.vue
+++ frontend/src/components/Contacts.vue
@@ -1,20 +1,59 @@
 <template lang="pug">
-div {{ contacts }}
+QTable(
+  title="Treats"
+  :rows="rows"
+  :columns="columns"
+)
 </template>
 
 <script lang="ts">
 import { defineComponent, computed } from 'vue';
 import { useStore } from 'src/store';
-import { ContactMap } from 'src/store/module-example/state';
+import { ContactMap, Contact } from 'src/store/module-example/state';
+import { QTable } from 'quasar';
+import * as _ from 'lodash';
 
 export default defineComponent({
   name: 'Contacts',
+  components: {
+    QTable,
+  },
   setup() {
     const store = useStore();
     void store.dispatch('example/fetchContacts');
     const contacts = computed((): ContactMap|null => store.state.example.contacts);
+    const rows = computed(() => _.values(contacts.value));
+    const columns = [
+      {
+        name: 'Id',
+        required: true,
+        label: 'Id',
+        align: 'left',
+        field: (row: Contact) => row.Id,
+        format: (val: string) => `${val}`,
+        sortable: true,
+      },
+      {
+        name: 'Username',
+        label: 'Username',
+        align: 'left',
+        field: (row: Contact) => row.Username,
+        format: (val: string) => `${val}`,
+        sortable: true,
+      },
+      {
+        name: 'Phone',
+        label: 'Phone',
+        align: 'left',
+        field: (row: Contact) => row.Phone,
+        format: (val: string) => `${val}`,
+        sortable: true,
+      },
+    ];
     return {
+      columns,
       contacts,
+      rows,
     };
   },
 });
