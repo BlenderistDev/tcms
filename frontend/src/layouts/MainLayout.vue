9950735c3269c4ffaba39562b483883b5fd9ca46
blenderistDev <demonx.blenderist@gmail.com>
2021-09-15 20:12:08 +0300

    Переписал шаблон Layout на pug

--- frontend/src/layouts/MainLayout.vue
+++ frontend/src/layouts/MainLayout.vue
@@ -1,50 +1,35 @@
-<template>
-  <q-layout view="lHh Lpr lFf">
-    <q-header elevated>
-      <q-toolbar>
-        <q-btn
-          flat
-          dense
-          round
-          icon="menu"
-          aria-label="Menu"
-          @click="toggleLeftDrawer"
-        />
-
-        <q-toolbar-title>
-          Quasar App
-        </q-toolbar-title>
-
-        <div>Quasar v{{ $q.version }}</div>
-      </q-toolbar>
-    </q-header>
-
-    <q-drawer
-      v-model="leftDrawerOpen"
-      show-if-above
-      bordered
-      class="bg-grey-1"
-    >
-      <q-list>
-        <q-item-label
-          header
-          class="text-grey-8"
-        >
-          Essential Links
-        </q-item-label>
-
-        <EssentialLink
-          v-for="link in essentialLinks"
-          :key="link.title"
-          v-bind="link"
-        />
-      </q-list>
-    </q-drawer>
-
-    <q-page-container>
-      <router-view />
-    </q-page-container>
-  </q-layout>
+<template lang="pug">
+QLayout(view="lHh Lpr lFf")
+  QHeader(elevated)
+    QToolbar
+      QBtn(
+        flat
+        dense
+        round
+        icon="menu"
+        aria-label="Menu"
+        @click="toggleLeftDrawer"
+      )
+      QToolbarTitle Quasar App
+      div Quasar v{{ $q.version }}
+  QDrawer(
+    v-model="leftDrawerOpen"
+    show-if-above
+    bordered
+    class="bg-grey-1"
+  )
+    QList
+      QItemLabel(
+        header
+        class="text-grey-8"
+      ) Essential Links
+      EssentialLink(
+        v-for="link in essentialLinks"
+        :key="link.title"
+        v-bind="link"
+      )
+  QPageContainer
+    router-view
 </template>
 
 <script lang="ts">
