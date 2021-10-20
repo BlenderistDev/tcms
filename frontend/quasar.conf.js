b652fe12cd70bba29c4881720420840221b4b78d
blenderistDev <demonx.blenderist@gmail.com>
2021-10-20 22:16:11 +0300

    Начало работы над обработкой обновлений телеграмма на фронтенде

--- frontend/quasar.conf.js
+++ frontend/quasar.conf.js
@@ -31,6 +31,7 @@ module.exports = configure((ctx) => ({
   // https://v2.quasar.dev/quasar-cli/boot-files
   boot: [
     'axios',
+    'websocket'
   ],
 
   // https://v2.quasar.dev/quasar-cli/quasar-conf-js#Property%3A-css
