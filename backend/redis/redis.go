bddec977f4f3c675718376ab2d80a1b278e295dd
blenderistDev <demonx.blenderist@gmail.com>
2021-10-25 19:43:10 +0300

    Приведение нагрузки к корректному формату перед публикацией в топик Redis

--- backend/redis/redis.go
+++ backend/redis/redis.go
@@ -2,12 +2,13 @@ package redis
 
 import (
 	"context"
+	"encoding/json"
 	"github.com/go-redis/redis/v8"
 	"tcms/m/dry"
 )
 
 type Client interface {
-	Publish(ctx context.Context, channel string, payload interface{})
+	Publish(ctx context.Context, channel string, payload interface{}) (*redis.IntCmd, error)
 	Subscribe(ctx context.Context, channel string) *redis.PubSub
 }
 
@@ -15,8 +16,12 @@ type client struct {
 	client *redis.Client
 }
 
-func (c client) Publish(ctx context.Context, channel string, payload interface{}) {
-	c.client.Publish(ctx, channel, payload)
+func (c client) Publish(ctx context.Context, channel string, payload interface{}) (*redis.IntCmd, error) {
+	data, err := json.Marshal(payload)
+	if err != nil {
+		return nil, err
+	}
+	return c.client.Publish(ctx, channel, data), nil
 }
 
 func (c client) Subscribe(ctx context.Context, channel string) *redis.PubSub {
