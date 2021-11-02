db3cb836948bbb3820771c12bf987ac0ce103166
blenderistDev <demonx.blenderist@gmail.com>
2021-11-02 21:45:29 +0300

    Event data parsing begin

--- backend/telegramClient/eventRecognize.go
+++ backend/telegramClient/eventRecognize.go
@@ -1,6 +1,7 @@
 package telegramClient
 
 import (
+	"fmt"
 	"github.com/xelaj/mtproto/telegram"
 	"reflect"
 	"strings"
@@ -13,19 +14,21 @@ func recognizeTrigger(i interface{}) []automation.TelegramUpdateTrigger {
 	switch message := i.(type) {
 	case *telegram.UpdateShort:
 		triggerType = getTriggerType(message.Update)
+		triggerData := getTriggerData(message.Update)
 		trigger := automation.TelegramUpdateTrigger{
 			Name:    triggerType,
 			KeyList: nil,
-			Data:    nil,
+			Data:    triggerData,
 		}
 		triggerList = append(triggerList, trigger)
 	case *telegram.UpdatesObj:
 		for _, event := range message.Updates {
 			triggerType = getTriggerType(event)
+			triggerData := getTriggerData(event)
 			trigger := automation.TelegramUpdateTrigger{
 				Name:    triggerType,
 				KeyList: nil,
-				Data:    event,
+				Data:    triggerData,
 			}
 			triggerList = append(triggerList, trigger)
 		}
@@ -39,4 +42,21 @@ func recognizeTrigger(i interface{}) []automation.TelegramUpdateTrigger {
 
 func getTriggerType(i interface{}) string {
 	return strings.Replace(reflect.TypeOf(i).String(), "*telegram.", "", 1)
+}
+
+func getTriggerData(i interface{}) map[string]interface{} {
+
+	v := reflect.ValueOf(i)
+
+	values := make([]interface{}, reflect.Indirect(v).NumField())
+
+	for i := 0; i < reflect.Indirect(v).NumField(); i++ {
+		values[i] = reflect.Indirect(v).Field(i).Interface()
+	}
+
+	fmt.Printf("%v", values)
+
+	var inInterface map[string]interface{}
+
+	return inInterface
 }
