d9a681b8368b7a28c38af0f9bf2da3a33d48ee8e
blenderistDev <demonx.blenderist@gmail.com>
2021-11-05 19:51:54 +0300

    recognizer refactoring

--- backend/telegramClient/eventRecognize.go
+++ backend/telegramClient/eventRecognize.go
@@ -2,8 +2,10 @@ package telegramClient
 
 import (
 	"fmt"
+	"github.com/k0kubun/pp"
 	"github.com/xelaj/mtproto/telegram"
 	"reflect"
+	"strconv"
 	"strings"
 	"tcms/m/automation"
 )
@@ -14,7 +16,7 @@ func recognizeTrigger(i interface{}) []automation.TelegramUpdateTrigger {
 	switch message := i.(type) {
 	case *telegram.UpdateShort:
 		triggerType = getTriggerType(message.Update)
-		triggerData := getTriggerData(message.Update)
+		triggerData := parsePtr(message.Update)
 		trigger := automation.TelegramUpdateTrigger{
 			Name: triggerType,
 			Data: triggerData,
@@ -23,7 +25,7 @@ func recognizeTrigger(i interface{}) []automation.TelegramUpdateTrigger {
 	case *telegram.UpdatesObj:
 		for _, event := range message.Updates {
 			triggerType = getTriggerType(event)
-			triggerData := getTriggerData(event)
+			triggerData := parsePtr(event)
 			trigger := automation.TelegramUpdateTrigger{
 				Name: triggerType,
 				Data: triggerData,
@@ -35,6 +37,7 @@ func recognizeTrigger(i interface{}) []automation.TelegramUpdateTrigger {
 		triggerType = "unknown"
 	}
 
+	pp.Println(triggerList)
 	return triggerList
 }
 
@@ -42,7 +45,110 @@ func getTriggerType(i interface{}) string {
 	return strings.Replace(reflect.TypeOf(i).String(), "*telegram.", "", 1)
 }
 
-func getTriggerData(i interface{}, prefixArr ...string) map[string]interface{} {
+func parsePtr(i interface{}, prefixArr ...string) map[string]interface{} {
+	if i == nil {
+		return nil
+	}
+	if reflect.ValueOf(i).IsNil() || reflect.ValueOf(i).IsZero() {
+		return nil
+	}
+
+	prefix := getPrefix(prefixArr...)
+
+	v := reflect.Indirect(reflect.ValueOf(i))
+
+	values := make(map[string]interface{}, v.NumField())
+
+	for i := 0; i < v.NumField(); i++ {
+		fieldData := v.Field(i).Interface()
+		filedName := v.Type().Field(i).Name
+		data := parseUnknown(fieldData, filedName)
+		for key, value := range data {
+			values[prefix+key] = value
+		}
+		//switch message := fieldData.(type) {
+		//case interface{}:
+		//	valueType := v.Field(i).Kind()
+		//
+		//	switch valueType {
+		//	case reflect.Interface:
+		//		data := parsePtr(message, v.Type().Field(i).Name)
+		//		for key, value := range data {
+		//			values[prefix+key] = value
+		//		}
+		//	case reflect.Slice:
+		//			fmt.Println("SSSSSLLLLLIIIIICCCCCEEEEE")
+		//			data := parseSlice(message, v.Type().Field(i).Name)
+		//			pp.Println(message)
+		//			pp.Println(data)
+		//		fmt.Println("SSSSSLLLLLIIIIICCCCCEEEEE22222")
+		//			for key, value := range data {
+		//				values[prefix+key] = value
+		//			}
+		//
+		//
+		//		//fmt.Println("11111slice11111")
+		//		//pp.Println(message)
+		//		//fmt.Println("22222slice22222")
+		//	case reflect.Ptr:
+		//		fmt.Println("ptr")
+		//	default:
+		//		values[prefix+v.Type().Field(i).Name] = message
+		//	}
+		//case []interface{}:
+		//	if v.Field(i).IsZero() || v.Field(i).IsNil() {
+		//		values[prefix+v.Type().Field(i).Name] = nil
+		//	} else {
+		//		fmt.Println("slice")
+		//	}
+		//default:
+		//	values[prefix+v.Type().Field(i).Name] = v.Field(i).Interface()
+		//}
+	}
+	return values
+}
+
+func parseUnknown(i interface{}, prefixArr ...string) map[string]interface{} {
+	if i == nil {
+		return nil
+	}
+
+	prefix := getPrefix(prefixArr...)
+
+	values := make(map[string]interface{})
+
+	valueType := reflect.ValueOf(i).Kind()
+
+	switch valueType {
+	case reflect.Ptr:
+		data := parsePtr(i)
+		for key, value := range data {
+			values[prefix+"."+key] = value
+		}
+	case reflect.Slice:
+		fmt.Println("slice")
+		data := parseSlice(i)
+		fmt.Println(data)
+		if len(data) > 0 {
+			for key, value := range data {
+				values[prefix+"."+key] = value
+			}
+		} else {
+			values[prefix] = nil
+		}
+
+	case reflect.Map:
+	case reflect.Array:
+	case reflect.Struct:
+
+	default:
+		values[prefix] = i
+	}
+
+	return values
+}
+
+func parseSlice(i interface{}, prefixArr ...string) map[string]interface{} {
 
 	if i == nil {
 		return nil
@@ -58,31 +164,23 @@ func getTriggerData(i interface{}, prefixArr ...string) map[string]interface{} {
 		prefix += "."
 	}
 
-	v := reflect.Indirect(reflect.ValueOf(i))
+	listVal := reflect.ValueOf(i)
 
-	values := make(map[string]interface{}, v.NumField())
+	values := make(map[string]interface{}, listVal.Len())
 
-	for i := 0; i < v.NumField(); i++ {
-
-		valueType := v.Field(i).Kind().String()
-
-		switch valueType {
-		case "interface":
-			data := getTriggerData(v.Field(i).Interface(), v.Type().Field(i).Name)
-			for key, value := range data {
-				values[prefix+key] = value
-			}
-		case "slice":
-			if v.Field(i).IsZero() || v.Field(i).IsNil() {
-				values[prefix+v.Type().Field(i).Name] = nil
-			} else {
-				fmt.Println("slice")
-			}
-		case "ptr":
-			fmt.Println("ptr")
-		default:
-			values[prefix+v.Type().Field(i).Name] = v.Field(i).Interface()
-		}
+	for key := 0; key < listVal.Len(); key++ {
+		values[prefix+strconv.Itoa(key)] = parseUnknown(listVal.Interface())
 	}
+
 	return values
+}
+
+func getPrefix(prefixArr ...string) string {
+	prefix := ""
+
+	if len(prefixArr) > 0 {
+		prefix = prefixArr[0]
+	}
+
+	return prefix
 }
