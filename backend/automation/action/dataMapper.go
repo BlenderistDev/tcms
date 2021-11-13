0cca7562cea3916feea775d6ec7f75935f0f57f6
blenderistDev <demonx.blenderist@gmail.com>
2021-11-14 00:36:53 +0300

    Move action data mapping to separate struct

---
+++ backend/automation/action/dataMapper.go
@@ -0,0 +1,45 @@
+package action
+
+import (
+	"fmt"
+	"strconv"
+	"tcms/m/automation/core"
+	"tcms/m/db/model"
+)
+
+type DataMapper struct {
+	Action model.Action
+}
+
+func (a DataMapper) getFromMapInt(trigger core.Trigger, key string) (int, error) {
+	s, err := a.getFromMap(trigger, key)
+	if err != nil {
+		return 0, err
+	}
+
+	i, err := strconv.Atoi(s)
+	if err != nil {
+		return 0, err
+	}
+
+	return i, nil
+}
+
+func (a DataMapper) getFromMap(trigger core.Trigger, key string) (string, error) {
+	mappingData, ok := a.Action.Mapping[key]
+	if ok {
+		if mappingData.Simple {
+			return mappingData.Value, nil
+		} else {
+			triggerData := trigger.GetData()
+			value, ok := triggerData[mappingData.Value]
+			if ok {
+				return value, nil
+			} else {
+				return "", fmt.Errorf("key %s not found in trigger data", key)
+			}
+		}
+
+	}
+	return "", fmt.Errorf("key %s not found", key)
+}
--- backend/automation/action/sendMessage.go
+++ backend/automation/action/sendMessage.go
@@ -2,7 +2,6 @@ package action
 
 import (
 	"fmt"
-	"strconv"
 	"tcms/m/automation/core"
 	"tcms/m/db/model"
 	"tcms/m/telegramClient"
@@ -10,14 +9,15 @@ import (
 
 type sendMessageAction struct {
 	telegram *telegramClient.TelegramClient
-	action   model.Action
+	DataMapper
 }
 
 func CreateSendMessageAction(action model.Action) core.Action {
-	telegram := telegramClient.NewTelegram()
 	return sendMessageAction{
-		telegram: telegram,
-		action:   action,
+		telegram: telegramClient.NewTelegram(),
+		DataMapper: DataMapper{
+			Action: action,
+		},
 	}
 }
 
@@ -45,35 +45,35 @@ func (a sendMessageAction) Execute(trigger core.Trigger) error {
 	return nil
 }
 
-func (a sendMessageAction) getFromMapInt(trigger core.Trigger, key string) (int, error) {
-	s, err := a.getFromMap(trigger, key)
-	if err != nil {
-		return 0, err
-	}
-
-	i, err := strconv.Atoi(s)
-	if err != nil {
-		return 0, err
-	}
-
-	return i, nil
-}
-
-func (a sendMessageAction) getFromMap(trigger core.Trigger, key string) (string, error) {
-	mappingData, ok := a.action.Mapping[key]
-	if ok {
-		if mappingData.Simple {
-			return mappingData.Value, nil
-		} else {
-			triggerData := trigger.GetData()
-			value, ok := triggerData[mappingData.Value]
-			if ok {
-				return value, nil
-			} else {
-				return "", fmt.Errorf("key %s not found in trigger data", key)
-			}
-		}
-
-	}
-	return "", fmt.Errorf("key %s not found", key)
-}
+//func (a sendMessageAction) getFromMapInt(trigger core.Trigger, key string) (int, error) {
+//	s, err := a.getFromMap(trigger, key)
+//	if err != nil {
+//		return 0, err
+//	}
+//
+//	i, err := strconv.Atoi(s)
+//	if err != nil {
+//		return 0, err
+//	}
+//
+//	return i, nil
+//}
+//
+//func (a sendMessageAction) getFromMap(trigger core.Trigger, key string) (string, error) {
+//	mappingData, ok := a.Action.Mapping[key]
+//	if ok {
+//		if mappingData.Simple {
+//			return mappingData.Value, nil
+//		} else {
+//			triggerData := trigger.GetData()
+//			value, ok := triggerData[mappingData.Value]
+//			if ok {
+//				return value, nil
+//			} else {
+//				return "", fmt.Errorf("key %s not found in trigger data", key)
+//			}
+//		}
+//
+//	}
+//	return "", fmt.Errorf("key %s not found", key)
+//}
