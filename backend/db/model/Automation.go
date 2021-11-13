898d2a71c2061efa4a3f6d6e881868402822a880
blenderistDev <demonx.blenderist@gmail.com>
2021-11-13 23:23:43 +0300

    Models for automation action mapping

--- backend/db/model/Automation.go
+++ backend/db/model/Automation.go
@@ -1,7 +1,18 @@
 package model
 
+type Mapping struct {
+	Simple bool   `bson:"simple"`
+	Name   string `bson:"name"`
+	Value  string `bson:"value"`
+}
+
+type Action struct {
+	Name    string             `bson:"name"`
+	Mapping map[string]Mapping `bson:"mapping"`
+}
+
 type Automation struct {
-	Id       string `bson:"_id"`
-	Triggers []string
-	Actions  []string
+	Id       string   `bson:"_id"`
+	Triggers []string `bson:"triggers"`
+	Actions  []Action `bson:"actions"`
 }
