aa1b4108236cab8bd6d429877b2d1a006c681914
blenderistDev <demonx.blenderist@gmail.com>
2021-11-24 23:30:49 +0300

    add condition support for automation core

--- backend/automation/core/core.go
+++ backend/automation/core/core.go
@@ -1,6 +1,9 @@
 package core
 
-import "fmt"
+import (
+	"fmt"
+	"tcms/m/dry"
+)
 
 type Trigger interface {
 	GetName() string
@@ -11,11 +14,31 @@ type Action interface {
 	Execute(trigger Trigger) error
 }
 
+type Condition interface {
+	Check(trigger Trigger) (bool, error)
+}
+
 type Automation struct {
-	Actions []Action
+	Actions   []Action
+	Condition Condition
 }
 
 func (a Automation) Execute(trigger Trigger) {
+	if a.Condition == nil || a.checkCondition(trigger) {
+		a.executeActions(trigger)
+	}
+}
+
+func (a Automation) checkCondition(trigger Trigger) bool {
+	res, err := a.Condition.Check(trigger)
+	if err != nil {
+		dry.HandleError(err)
+		return false
+	}
+	return res
+}
+
+func (a Automation) executeActions(trigger Trigger) {
 	for _, action := range a.Actions {
 		err := action.Execute(trigger)
 		if err != nil {
