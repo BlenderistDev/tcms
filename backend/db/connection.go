aec85b3517ef4b51417ea53a4b730a536960523f
blenderistDev <demonx.blenderist@gmail.com>
2021-11-26 22:51:42 +0300

    refactoring

--- backend/db/connection.go
+++ backend/db/connection.go
@@ -8,7 +8,8 @@ import (
 )
 
 func GetConnection(ctx context.Context) *mongo.Database {
-	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://127.0.0.1:27017"))
+	const url = "mongodb://127.0.0.1:27017"
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(url))
 	if err != nil {
 		dry.HandleErrorPanic(err)
 	}
