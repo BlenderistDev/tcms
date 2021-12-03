2fd30d44b91129890aec845110b062aca97e3704
blenderistDev <demonx.blenderist@gmail.com>
2021-12-03 19:48:25 +0300

    add getRedisDatabase test

--- backend/internal/redis/env_test.go
+++ backend/internal/redis/env_test.go
@@ -2,6 +2,7 @@ package redis
 
 import (
 	"os"
+	"strconv"
 	"tcms/m/internal/dry"
 	"testing"
 )
@@ -28,4 +29,21 @@ func TestGetRedisPasswordEmpty(t *testing.T) {
 	os.Clearenv()
 	result := getRedisPassword()
 	dry.TestCheckEqual(t, "", result)
+}
+
+func TestGetRedisDatabase(t *testing.T) {
+	database := 1
+	databaseStr := strconv.Itoa(database)
+	err := os.Setenv("REDIS_DATABASE", databaseStr)
+	dry.TestHandleError(t, err)
+	result, err := getRedisDatabase()
+	dry.TestHandleError(t, err)
+	dry.TestCheckEqual(t, database, result)
+}
+
+func TestGetRedisDatabaseEmpty(t *testing.T) {
+	os.Clearenv()
+	result, err := getRedisDatabase()
+	dry.TestHandleError(t, err)
+	dry.TestCheckEqual(t, 0, result)
 }
