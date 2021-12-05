d4e5ce7f771b31862e377ee819ad3bf863f42c4d
blenderistDev <demonx.blenderist@gmail.com>
2021-12-06 00:56:15 +0300

    add test for sendMessage action

--- backend/internal/automation/action/sendMessage_test.go
+++ backend/internal/automation/action/sendMessage_test.go
@@ -2,6 +2,7 @@ package action
 
 import (
 	"github.com/golang/mock/gomock"
+	"tcms/m/internal/automation/core"
 	"tcms/m/internal/db/model"
 	"tcms/m/internal/dry"
 	telegramClient2 "tcms/m/internal/telegramClient/mock"
@@ -34,5 +35,47 @@ func TestCreateSendMessageAction(t *testing.T) {
 }
 
 func TestSendMessageAction_Execute(t *testing.T) {
+	const (
+		messageKey      = "message"
+		messageValue    = "test message"
+		accessHashKey   = "accessHash"
+		accessHashValue = "456456"
+		peerKey         = "peer"
+		peerValue       = "123123"
+	)
 
+	var peerValueInt int32 = 123123
+	var accessHashValueInt int64 = 456456
+
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	telegramClient := telegramClient2.NewMockTelegramClient(ctrl)
+	telegramClient.EXPECT().SendMessage(gomock.Eq(messageValue), gomock.Eq(peerValueInt), gomock.Eq(accessHashValueInt))
+
+	actionModel := model.Action{
+		Name: "name",
+		Mapping: map[string]model.Mapping{
+			peerKey: {
+				Simple: true,
+				Name:   peerKey,
+				Value:  peerValue,
+			},
+			accessHashKey: {
+				Simple: true,
+				Name:   accessHashKey,
+				Value:  accessHashValue,
+			},
+			messageKey: {
+				Simple: true,
+				Name:   messageKey,
+				Value:  messageValue,
+			},
+		},
+	}
+
+	trigger := core.NewMockTrigger(ctrl)
+	sendMessageAction := createSendMessageAction(actionModel, telegramClient)
+	err := sendMessageAction.Execute(trigger)
+	dry.TestHandleError(t, err)
 }
