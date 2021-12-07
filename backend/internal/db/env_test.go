45ca808dd91d5cee0b4eedbc69d3c0e12727f314
blenderistDev <demonx.blenderist@gmail.com>
2021-12-07 20:00:48 +0300

    add test for database config

--- backend/internal/db/env_test.go
+++ backend/internal/db/env_test.go
@@ -16,3 +16,9 @@ func TestGetMongoHost(t *testing.T) {
 		t.Errorf("expect %s, got %s", host, result)
 	}
 }
+
+func TestGetMongoHost_notExist(t *testing.T) {
+	os.Clearenv()
+	_, err := getMongoHost()
+	dry.TestCheckEqual(t, "no mongodb host", err.Error())
+}
