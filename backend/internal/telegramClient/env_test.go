c1a1b66b5cba3da98354b8d7695615e0b5f1667b
blenderistDev <demonx.blenderist@gmail.com>
2021-12-08 22:23:29 +0300

    add test for telegram env

---
+++ backend/internal/telegramClient/env_test.go
@@ -0,0 +1,16 @@
+package telegramClient
+
+import (
+	"os"
+	"tcms/m/internal/dry"
+	"testing"
+)
+
+func TestGetMTProtoHost(t *testing.T) {
+	host := "127.0.0.1:1111"
+	err := os.Setenv("MTPROTO_HOST", host)
+	dry.TestHandleError(t, err)
+	result, err := getMTProtoHost()
+	dry.TestHandleError(t, err)
+	dry.TestCheckEqual(t, host, result)
+}
