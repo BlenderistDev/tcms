1bf4d4fd4fedb2bb9809a7f8acf6c66a0be4bddd
blenderistDev <demonx.blenderist@gmail.com>
2021-12-09 21:54:43 +0300

    telegram env test refactoring

--- backend/internal/dry/testing.go
+++ backend/internal/dry/testing.go
@@ -1,6 +1,7 @@
 package dry
 
 import (
+	"os"
 	"reflect"
 	"testing"
 )
@@ -15,4 +16,17 @@ func TestCheckEqual(t *testing.T, expected, testing interface{}) {
 	if !reflect.DeepEqual(expected, testing) {
 		t.Errorf("expect %v, got %v", expected, testing)
 	}
+}
+
+func TestEnvString(t *testing.T, name string, error string, f func() (string, error)) {
+	value := "testing"
+	err := os.Setenv(name, value)
+	TestHandleError(t, err)
+	result, err := f()
+	TestHandleError(t, err)
+	TestCheckEqual(t, value, result)
+
+	os.Clearenv()
+	_, err = f()
+	TestCheckEqual(t, error, err.Error())
 }
--- backend/internal/telegramClient/env_test.go
+++ backend/internal/telegramClient/env_test.go
@@ -1,32 +1,10 @@
 package telegramClient
 
 import (
-	"os"
 	"tcms/m/internal/dry"
 	"testing"
 )
 
 func TestGetMTProtoHost(t *testing.T) {
-	host := "127.0.0.1:1111"
-	err := os.Setenv("MTPROTO_HOST", host)
-	dry.TestHandleError(t, err)
-	result, err := getMTProtoHost()
-	dry.TestHandleError(t, err)
-	dry.TestCheckEqual(t, host, result)
-}
-
-func TestGetMTProtoHost_notExist(t *testing.T) {
-	os.Clearenv()
-	_, err := getMTProtoHost()
-	dry.TestCheckEqual(t, "no mtproto host", err.Error())
-}
-
-func TestGetAppId(t *testing.T) {
-	appId := "123123"
-	appIdInt := 123123
-	err := os.Setenv("TELEGRAM_APP_ID", appId)
-	dry.TestHandleError(t, err)
-	result, err := getAppId()
-	dry.TestHandleError(t, err)
-	dry.TestCheckEqual(t, appIdInt, result)
+	dry.TestEnvString(t, "MTPROTO_HOST", "no mtproto host", getMTProtoHost)
 }
