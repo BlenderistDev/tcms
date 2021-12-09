c68581847ab9830720fbb1908a400bd54a63090c
blenderistDev <demonx.blenderist@gmail.com>
2021-12-09 21:55:37 +0300

    mongodb env test refactoring

--- backend/internal/db/env_test.go
+++ backend/internal/db/env_test.go
@@ -1,24 +1,10 @@
 package db
 
 import (
-	"os"
 	"tcms/m/internal/dry"
 	"testing"
 )
 
 func TestGetMongoHost(t *testing.T) {
-	host := "127.0.0.1:1111"
-	err := os.Setenv("MONGO_HOST", host)
-	dry.TestHandleError(t, err)
-	result, err := getMongoHost()
-	dry.TestHandleError(t, err)
-	if result != host {
-		t.Errorf("expect %s, got %s", host, result)
-	}
-}
-
-func TestGetMongoHost_notExist(t *testing.T) {
-	os.Clearenv()
-	_, err := getMongoHost()
-	dry.TestCheckEqual(t, "no mongodb host", err.Error())
+	dry.TestEnvString(t, "MONGO_HOST", "no mongodb host", getMongoHost)
 }
