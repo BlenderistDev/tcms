018978c09d169d151a1c5d19496b6965833ecb74
blenderistDev <demonx.blenderist@gmail.com>
2021-12-10 21:53:38 +0300

    add telegram env GetAppHash test

--- backend/internal/telegramClient/env_test.go
+++ backend/internal/telegramClient/env_test.go
@@ -10,5 +10,9 @@ func TestGetMTProtoHost(t *testing.T) {
 }
 
 func TestGetAppId(t *testing.T) {
-	dry.TestEnvIntWithError(t, "APP_ID", "no app id", getAppId)
+	dry.TestEnvIntWithError(t, "TELEGRAM_APP_ID", "no app key", getAppId)
+}
+
+func TestGetAppHash(t *testing.T) {
+	dry.TestEnvString(t, "TELEGRAM_APP_HASH", "no app hash", getAppHash)
 }
