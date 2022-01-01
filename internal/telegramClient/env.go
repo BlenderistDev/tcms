d3816d855a9178bf846e1c3dd91ebfc44fbfcdff
blenderistDev <demonx.blenderist@gmail.com>
2022-01-01 13:18:23 +0300

    remove telegram env

--- internal/telegramClient/env.go
+++ internal/telegramClient/env.go
@@ -4,18 +4,6 @@ import (
 	"tcms/m/internal/dry"
 )
 
-func getMTProtoHost() (string, error) {
-	return dry.GetEnvStr("MTPROTO_HOST")
-}
-
-func getAppId() (int, error) {
-	return dry.GetEnvIntWithDefault("TELEGRAM_APP_ID", 0)
-}
-
-func getAppHash() (string, error) {
-	return dry.GetEnvStr("TELEGRAM_APP_HASH")
-}
-
 func getTelegramBridgeHost() (string, error) {
 	return dry.GetEnvStr("TELEGRAM_BRIDGE_HOST")
 }
--- internal/telegramClient/env_test.go
+++ internal/telegramClient/env_test.go
@@ -5,18 +5,6 @@ import (
 	"testing"
 )
 
-func TestGetMTProtoHost(t *testing.T) {
-	dry.TestEnvString(t, "MTPROTO_HOST", getMTProtoHost)
-}
-
-func TestGetAppId(t *testing.T) {
-	dry.TestEnvIntWithDefault(t, "TELEGRAM_APP_ID", 0, getAppId)
-}
-
-func TestGetAppHash(t *testing.T) {
-	dry.TestEnvString(t, "TELEGRAM_APP_HASH", getAppHash)
-}
-
 func TestGetTelegramBridgeHost(t *testing.T) {
 	dry.TestEnvString(t, "TELEGRAM_BRIDGE_HOST", getTelegramBridgeHost)
 }
