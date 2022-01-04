3c25ae225a0ec0427c59fe0bca28331237b0b9ef
blenderistDev <demonx.blenderist@gmail.com>
2022-01-05 00:33:59 +0300

    add create test for not condition

--- internal/automation/condition/not_test.go
+++ internal/automation/condition/not_test.go
@@ -9,7 +9,24 @@ import (
 	"testing"
 )
 
-func TestNotCondition(t *testing.T) {
+func TestNotCondition_createNotCondition(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	subCondition := mock_interfaces.NewMockCondition(ctrl)
+
+	subConditions := []interfaces.Condition{subCondition}
+	createdCondition, err := createNotCondition(datamapper.DataMapper{}, subConditions)
+	dry.TestHandleError(t, err)
+
+	switch condition := createdCondition.(type) {
+	case notCondition:
+		dry.TestCheckEqual(t, subCondition, condition.subCondition)
+	default:
+		t.Errorf("condition type is not notCondition")
+	}
+}
+
+func TestNotCondition_CheckWithTrueSubcondition(t *testing.T) {
 	ctrl := gomock.NewController(t)
 	defer ctrl.Finish()
 	trigger := mock_interfaces.NewMockTrigger(ctrl)
