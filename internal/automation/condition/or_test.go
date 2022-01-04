a9a6e457b04c5baca7194397ee2c97060b0b7532
blenderistDev <demonx.blenderist@gmail.com>
2022-01-05 01:02:18 +0300

    add create error test for or condition

--- internal/automation/condition/or_test.go
+++ internal/automation/condition/or_test.go
@@ -26,3 +26,12 @@ func TestOrCondition_createOrCondition(t *testing.T) {
 		t.Errorf("condition type is not orCondition")
 	}
 }
+
+func TestOrCondition_createOrCondition_withLessConditions(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	subCondition := mock_interfaces.NewMockCondition(ctrl)
+	subConditions := []interfaces.Condition{subCondition}
+	_, err := createOrCondition(datamapper.DataMapper{}, subConditions)
+	dry.TestCheckEqual(t, "or condition should have at least two subconditions", err.Error())
+}
