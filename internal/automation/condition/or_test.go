4fb05baed22e542207db0d825b977542f7e41cd9
blenderistDev <demonx.blenderist@gmail.com>
2022-01-05 01:00:20 +0300

    add create test for or condition

---
+++ internal/automation/condition/or_test.go
@@ -0,0 +1,28 @@
+package condition
+
+import (
+	"github.com/golang/mock/gomock"
+	"tcms/m/internal/automation/datamapper"
+	"tcms/m/internal/automation/interfaces"
+	"tcms/m/internal/dry"
+	mock_interfaces "tcms/m/internal/testing/automation/interfaces"
+	"testing"
+)
+
+func TestOrCondition_createOrCondition(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	subCondition1 := mock_interfaces.NewMockCondition(ctrl)
+	subCondition2 := mock_interfaces.NewMockCondition(ctrl)
+
+	subConditions := []interfaces.Condition{subCondition1, subCondition2}
+	createdCondition, err := createOrCondition(datamapper.DataMapper{}, subConditions)
+	dry.TestHandleError(t, err)
+
+	switch condition := createdCondition.(type) {
+	case orCondition:
+		dry.TestCheckEqual(t, subConditions, condition.subConditions)
+	default:
+		t.Errorf("condition type is not orCondition")
+	}
+}
