8185d6b902cff0d62a448bfe73afbe42cbafa781
blenderistDev <demonx.blenderist@gmail.com>
2022-01-10 23:38:54 +0300

    add no id error test for muteChat action

--- internal/automation/action/muteChat_test.go
+++ internal/automation/action/muteChat_test.go
@@ -4,6 +4,7 @@ import (
 	"github.com/golang/mock/gomock"
 	"tcms/m/internal/db/model"
 	"tcms/m/internal/dry"
+	mock_interfaces "tcms/m/internal/testing/automation/interfaces"
 	telegramClient2 "tcms/m/internal/testing/telegramClient"
 	"testing"
 )
@@ -31,4 +32,32 @@ func TestCreateMuteChatAction(t *testing.T) {
 	default:
 		t.Errorf("action type is not muteChatAction")
 	}
+}
+
+func TestMuteUserAction_Execute_idError(t *testing.T) {
+	const (
+		unMuteKey   = "unMute"
+		unMuteValue = ""
+	)
+
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	telegramClient := telegramClient2.NewMockTelegramClient(ctrl)
+
+	actionModel := model.Action{
+		Name: "name",
+		Mapping: map[string]model.Mapping{
+			unMuteKey: {
+				Simple: true,
+				Name:   unMuteKey,
+				Value:  unMuteValue,
+			},
+		},
+	}
+
+	trigger := mock_interfaces.NewMockTrigger(ctrl)
+	muteUserAction := createMuteChatAction(actionModel, telegramClient)
+	err := muteUserAction.Execute(trigger)
+	dry.TestCheckEqual(t, "key id not found", err.Error())
 }
