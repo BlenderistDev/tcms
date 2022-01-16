bf816768b36cfc575b3094e6f28fdbbec6ded11c
blenderistDev <demonx.blenderist@gmail.com>
2022-01-16 14:23:35 +0300

    Revert "remove action factory"
    This reverts commit a9b104d199ca65a9e0b3e2b21e557def30e84985.

---
+++ internal/automation/action/factory_test.go
@@ -0,0 +1,85 @@
+package action
+
+import (
+	"github.com/golang/mock/gomock"
+	"tcms/m/internal/db/model"
+	"tcms/m/internal/dry"
+	telegramClient2 "tcms/m/internal/testing/telegramClient"
+	"testing"
+)
+
+func TestCreateAction_createSendMessage(t *testing.T) {
+	actionModel := model.Action{
+		Name:    "sendMessage",
+		Mapping: nil,
+	}
+
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	telegramClient := telegramClient2.NewMockTelegramClient(ctrl)
+
+	action, err := CreateAction(actionModel, telegramClient)
+	dry.TestHandleError(t, err)
+	switch action.(type) {
+	case sendMessageAction:
+	default:
+		t.Errorf("action type is not sendMessageAction")
+	}
+}
+
+func TestCreateAction_createMuteUser(t *testing.T) {
+	actionModel := model.Action{
+		Name:    "muteUser",
+		Mapping: nil,
+	}
+
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	telegramClient := telegramClient2.NewMockTelegramClient(ctrl)
+
+	action, err := CreateAction(actionModel, telegramClient)
+	dry.TestHandleError(t, err)
+	switch action.(type) {
+	case muteUserAction:
+	default:
+		t.Errorf("action type is not muteUserAction")
+	}
+}
+
+func TestCreateAction_createMuteChat(t *testing.T) {
+	actionModel := model.Action{
+		Name:    "muteChat",
+		Mapping: nil,
+	}
+
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	telegramClient := telegramClient2.NewMockTelegramClient(ctrl)
+
+	action, err := CreateAction(actionModel, telegramClient)
+	dry.TestHandleError(t, err)
+	switch action.(type) {
+	case muteChatAction:
+	default:
+		t.Errorf("action type is not muteUserAction")
+	}
+}
+
+func TestCreateAction_unknownAction(t *testing.T) {
+	const name = "someAction"
+	actionModel := model.Action{
+		Name:    name,
+		Mapping: nil,
+	}
+
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	telegramClient := telegramClient2.NewMockTelegramClient(ctrl)
+
+	_, err := CreateAction(actionModel, telegramClient)
+	dry.TestCheckEqual(t, "unknown action "+name, err.Error())
+}
