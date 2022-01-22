66aa4f67a0b7b26368e562cefa4cfaaae45fbc48
blenderistDev <demonx.blenderist@gmail.com>
2022-01-22 18:37:59 +0300

    fix equal condition tests

--- internal/automation/condition/equal_test.go
+++ internal/automation/condition/equal_test.go
@@ -4,20 +4,21 @@ import (
 	"fmt"
 	"github.com/golang/mock/gomock"
 	"tcms/m/internal/automation/datamapper"
-	"tcms/m/internal/db/model"
 	"tcms/m/internal/dry"
+	mock_datamapper "tcms/m/internal/testing/automation/datamapper"
 	mock_interfaces "tcms/m/internal/testing/automation/interfaces"
 	"testing"
 )
 
 func TestEqualCondition(t *testing.T) {
 
-	mapping := map[string]model.Mapping{
-		"test": {
-			Simple: true,
-			Name:   "name",
-			Value:  "value",
-		},
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	m := mock_datamapper.NewMockMapping(ctrl)
+
+	mapping := map[string]datamapper.Mapping{
+		"test": m,
 	}
 
 	dm := datamapper.DataMapper{Mapping: mapping}
@@ -34,21 +35,41 @@ func TestEqualCondition(t *testing.T) {
 func TestEqualCondition_Check(t *testing.T) {
 	ctrl := gomock.NewController(t)
 	defer ctrl.Finish()
-	trigger := mock_interfaces.NewMockTrigger(ctrl)
 
-	dm := datamapper.DataMapper{Mapping: map[string]model.Mapping{
-		"value1": {
-			Simple: true,
-			Name:   "value1",
-			Value:  "value",
-		},
-		"value2": {
-			Simple: true,
-			Name:   "value2",
-			Value:  "value",
-		},
-	}}
+	m1 := mock_datamapper.NewMockMapping(ctrl)
+	m1.
+		EXPECT().
+		IsSimple().
+		Return(true)
+
+	m1.
+		EXPECT().
+		GetValue().
+		Return("value")
+
+	m2 := mock_datamapper.NewMockMapping(ctrl)
+	m2.
+		EXPECT().
+		IsSimple().
+		Return(true)
+
+	m2.
+		EXPECT().
+		GetValue().
+		Return("value")
+
+	mapping := map[string]datamapper.Mapping{
+		"value1": m1,
+		"value2": m2,
+	}
+
+	trigger := mock_interfaces.NewMockTrigger(ctrl)
+	trigger.EXPECT().GetData().Return(make(map[string]string))
+	trigger.EXPECT().GetData().Return(make(map[string]string))
+
+	dm := datamapper.DataMapper{Mapping: mapping}
 	createdCondition := createEqualCondition(dm, nil)
+
 	res, err := createdCondition.Check(trigger)
 	dry.TestHandleError(t, err)
 	dry.TestCheckEqual(t, true, res)
@@ -58,14 +79,15 @@ func TestEqualCondition_Check_value1NotExist(t *testing.T) {
 	ctrl := gomock.NewController(t)
 	defer ctrl.Finish()
 	trigger := mock_interfaces.NewMockTrigger(ctrl)
+	trigger.EXPECT().GetData().Return(make(map[string]string))
 
-	dm := datamapper.DataMapper{Mapping: map[string]model.Mapping{
-		"value2": {
-			Simple: true,
-			Name:   "value2",
-			Value:  "value",
-		},
-	}}
+	m := mock_datamapper.NewMockMapping(ctrl)
+
+	mapping := map[string]datamapper.Mapping{
+		"value2": m,
+	}
+
+	dm := datamapper.DataMapper{Mapping: mapping}
 	createdCondition := createEqualCondition(dm, nil)
 	_, err := createdCondition.Check(trigger)
 	dry.TestCheckEqual(t, fmt.Sprintf("key %s not found", "value1"), err.Error())
@@ -75,14 +97,25 @@ func TestEqualCondition_Check_value2NotExist(t *testing.T) {
 	ctrl := gomock.NewController(t)
 	defer ctrl.Finish()
 	trigger := mock_interfaces.NewMockTrigger(ctrl)
+	trigger.EXPECT().GetData().Return(make(map[string]string))
+	trigger.EXPECT().GetData().Return(make(map[string]string))
 
-	dm := datamapper.DataMapper{Mapping: map[string]model.Mapping{
-		"value1": {
-			Simple: true,
-			Name:   "value1",
-			Value:  "value",
-		},
-	}}
+	m := mock_datamapper.NewMockMapping(ctrl)
+	m.
+		EXPECT().
+		IsSimple().
+		Return(true)
+
+	m.
+		EXPECT().
+		GetValue().
+		Return("value")
+
+	mapping := map[string]datamapper.Mapping{
+		"value1": m,
+	}
+
+	dm := datamapper.DataMapper{Mapping: mapping}
 	createdCondition := createEqualCondition(dm, nil)
 	_, err := createdCondition.Check(trigger)
 	dry.TestCheckEqual(t, fmt.Sprintf("key %s not found", "value2"), err.Error())
